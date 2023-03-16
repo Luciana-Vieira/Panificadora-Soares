d78dfe93a442b9223b6a1b5655c8eda2b3d9e58a
Luciana Vieira <logistic.lu@gmail.com>
2023-03-16 17:16:23 -0300

    PanificadoraSoares

---
+++ Codigo/PanificadoraSoares/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PanificadoraSoares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
